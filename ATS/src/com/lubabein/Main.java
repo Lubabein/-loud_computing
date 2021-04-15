58f9fa02f2dc51db55778beb6f127e414cbb8898
Lubabein <31561261+Lubabein@users.noreply.github.com>
2021-04-15 15:50:57 +0300

    Ats

--- ATS/src/com/lubabein/Main.java
+++ ATS/src/com/lubabein/Main.java
@@ -8,11 +8,10 @@ public class Main {
 	public static void main(String[] args) {
 		ATS ats = new ATS();
 		ats.startCall("121","665");
-        ats.startCall("333","666");
-        ats.startCall("111","666");   
-        ats.startCall("151","666");
-        ats.startCall("222","666");
-        
+        	ats.startCall("333","666");
+        	ats.startCall("111","666");   
+        	ats.startCall("151","666");
+        	ats.startCall("222","666");
 	}
 
 }
